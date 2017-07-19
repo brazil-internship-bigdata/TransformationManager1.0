911191311861d3d8ea37fa6867431fafcbda5d67
geourjoa <anthony.geourjon@gmail.com>
2017-07-19 14:40:19 -0300

    http instead of https

--- src/httpManaging/HttpManager.java
+++ src/httpManaging/HttpManager.java
@@ -1,6 +1,5 @@
 package httpManaging;
 
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -22,107 +21,110 @@ import main.Home;
 
 public class HttpManager {
 
-	private final String url = "https://localhost";
-	private final String savingsPath = "savings/API/ids";
-	
-	private String userName;
-	private String password;
-	
-	private boolean connected;
-	
-	
-	private class Result{
-		int code;
-		ArrayList<String> response;
+	private final String	url			= "http://localhost";
+	private final String	savingsPath	= "savings/API/ids";
+
+	private String			userName;
+	private String			password;
+
+	private boolean			connected;
+
+	private class Result {
+		int					code;
+		ArrayList<String>	response;
 
 		public Result(int code, ArrayList<String> response) {
 			this.code = code;
 			this.response = response;
 		}
-		
+
 		public ArrayList<String> response() {
 			return response;
 		}
+
 		public int code() {
 			return code;
 		}
 	}
-	
-	
-	
+
 	public HttpManager() {
-		//For now, we assume that the connection wasn't made. This might change if the ids are correctly stored in the savings File
+		// For now, we assume that the connection wasn't made. This might change
+		// if the ids are correctly stored in the savings File
 		connected = false;
 
 		Path path = Paths.get(savingsPath);
-		if( !Files.exists(path) ) {
+		if (!Files.exists(path)) {
 			return;
 		} else {
 
 			try {
 				List<String> content = Files.readAllLines(path);
-			
-				if(content.size() != 2) {
-					//The file's format isn't normal. => we are not connected
+
+				if (content.size() != 2) {
+					// The file's format isn't normal. => we are not connected
 					connected = false;
 					return;
 				}
-				
-				//Here, the savingsFiles exists, in the right format. So we use the ID and password to create this object
+
+				// Here, the savingsFiles exists, in the right format. So we use
+				// the ID and password to create this object
 				String idLine = content.get(0);
 				String pwLine = content.get(1);
-				
+
 				userName = idLine.replaceAll("ID=", "");
 				password = pwLine.replaceAll("PASSWORD=", "");
 				connected = true;
-				
+
 			} catch (IOException e) {
-				//If we don't manage to read the savings File, we are not connected.
+				// If we don't manage to read the savings File, we are not
+				// connected.
 				connected = false;
 				e.printStackTrace();
 				return;
 			}
-			
-			
+
 		}
 	}
-	
-	
-	
+
 	/**
 	 * Tests the connection ids to communicate with the API.
-	 * @param userName of the company
-	 * @param password of the company
-	 * @return true if the API returns a positive question to the company. false if the response isn't.
-	 * @throws IOException In case of failure during the communication with the API
+	 * 
+	 * @param userName
+	 *            of the company
+	 * @param password
+	 *            of the company
+	 * @return true if the API returns a positive question to the company. false
+	 *         if the response isn't.
+	 * @throws IOException
+	 *             In case of failure during the communication with the API
 	 */
 	public void connect(String userName, String password) throws IOException {
-		
-		String urlParameters = "connection?id=+"+userName+"&pw="+password;
+
+		String urlParameters = "id=+" + userName + "&pw=" + password;
 
 		Result res = post(urlParameters);
-			
-		if( res.code() != HttpURLConnection.HTTP_OK ) {
+
+		if (res.code() != HttpURLConnection.HTTP_OK) {
 			connected = false;
 			return;
 		}
-		
+
 		this.userName = userName;
 		this.password = password;
 
 		System.out.println("I most likely am not a good piece of code");
 
-		//write the ids to connect to the lab's API.
-		File savingsFile = new File(savingsPath);	
-		if( !savingsFile.exists() ) {
+		// write the ids to connect to the lab's API.
+		File savingsFile = new File(savingsPath);
+		if (!savingsFile.exists()) {
 			savingsFile.getParentFile().mkdirs();
 			savingsFile.createNewFile();
 		}
-		
+
 		PrintWriter printer = null;
 		try {
 			printer = new PrintWriter(savingsFile);
-			printer.print("ID="+userName+"\nPASSWORD="+password);
+			printer.print("ID=" + userName + "\nPASSWORD=" + password);
 		} catch (IOException e) {
 			System.err.println("I didn't manage to print the ids in a file.");
 			e.printStackTrace();
@@ -130,82 +132,82 @@ public class HttpManager {
 			printer.flush();
 			printer.close();
 		}
-		
+
 		connected = true;
 		return;
 	}
 
-
 	public boolean connected() {
 		return connected;
 	}
-	
-	
+
 	/**
 	 * post the item identifier and give back the result.
+	 * 
 	 * @param item
 	 * @return the result returned by the post.
-	 * @throws IOException if the post method failed
+	 * @throws IOException
+	 *             if the post method failed
 	 */
 	private Result postIdentifier(Item item) throws IOException {
 		String itemIdentifier = item.getIdentifier();
 		String companyName = Home.COMPANY_NAME;
-		
-		//TODO identifier is an ambiguous name, it should be replaced... ItemIdentifier?
-		String urlParameters = "transformation?company="+companyName+"&identifier="+itemIdentifier;
-		
+
+		// TODO identifier is an ambiguous name, it should be replaced...
+		// ItemIdentifier?
+		String urlParameters = "transformation?company=" + companyName + "&identifier=" + itemIdentifier;
+
 		Result res = post(urlParameters);
 		return res;
-		
+
 	}
 
-	
-	
 	/**
-	 * post the paths of the files necessary to the transformation. This way the API will upload them
-	 * @param item associated with this transformation
-	 * @throws IOException if one of the posts request failed.
+	 * post the paths of the files necessary to the transformation. This way the
+	 * API will upload them
+	 * 
+	 * @param item
+	 *            associated with this transformation
+	 * @throws IOException
+	 *             if one of the posts request failed.
 	 */
 	void retrieveTransformationFiles(Item item) throws IOException {
 		File dir = new File(item.transformationFolder());
-		if( !dir.isDirectory() ) {
+		if (!dir.isDirectory()) {
 			item.generateFolders();
 		}
-		
+
 		Result res = postIdentifier(item);
-		
+
 		ArrayList<String> filesToAsk = res.response;
-		
-		for(String filePath : filesToAsk) {
-			String parameters = "something" + filePath + connectionParameters();//TODO
+
+		for (String filePath : filesToAsk) {
+			String parameters = "something" + filePath + connectionParameters();// TODO
 
 			System.out.println("I am a bad piece of code");
-			
+
 			post(parameters);
 		}
-		
+
 	}
-	
-	
-	
-	
-	//TODO replace pseudo code
+
+	// TODO replace pseudo code
 	private String connectionParameters() {
 		return "something" + userName + "anotherThing" + password;
 	}
-
-
 
 	private Result post(String parameters) throws IOException {
 		URL obj = new URL(url);
 
 		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-		//add request header
+		// add request header
 		con.setRequestMethod("POST");
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-		//String urlParameters = "connection?id=+"+userName+"&pw="+password;// = "search?q=aa&oq=aa&aqs=chrome..69i57j0l5.970j0j7&client=ubuntu&sourceid=chrome&ie=UTF-8";
+		// String urlParameters = "connection?id=+"+userName+"&pw="+password;//
+		// =
+		// "search?q=aa&oq=aa&aqs=chrome..69i57j0l5.970j0j7&client=ubuntu&sourceid=chrome&ie=UTF-8";
 
 		// Send post request
 		con.setDoOutput(true);
@@ -220,8 +222,7 @@ public class HttpManager {
 		System.out.println("Post parameters : " + parameters);
 		System.out.println("Response Code : " + responseCode);
 
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(con.getInputStream()));
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		ArrayList<String> response = new ArrayList<String>();
 
@@ -230,11 +231,10 @@ public class HttpManager {
 		}
 		in.close();
 
-
-		//print result
-		for(String line : response) {
+		// print result
+		for (String line : response) {
 			System.out.println(line);
-			
+
 		}
 
 		return new Result(responseCode, response);
