1adf75d4345743a3b5a6ea08379e087bba7f6d85
geourjoa <anthony.geourjon@gmail.com>
2017-08-16 17:40:17 +0200

    Merge

--- src/main/Home.java
+++ src/main/Home.java
@@ -1,12 +1,13 @@
 package main;
+
 import java.awt.event.ContainerEvent;
 import java.awt.event.ContainerListener;
-
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 
+import dataManaging.AbstractDataList;
 import dataManaging.MyListView;
 import dataManaging.databaseConnections.sqlConnection.MySQLconnection;
 import dataManaging.databaseConnections.sqlConnection.MySQLconnectionSupplier;
@@ -14,73 +15,94 @@ import dataManaging.databaseConnections.sqlConnection.MySQLconnectionsList;
 import dataManaging.files.FileList;
 import dataManaging.files.MyFile;
 import dataManaging.files.MyFileSupplier;
+import httpManaging.ApiConnectionPane;
 
-
-public class Home extends JFrame
-						implements ContainerListener{
+public class Home extends JFrame implements ContainerListener {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -4611492777705564096L;
+	private static final long					serialVersionUID	= -4611492777705564096L;
 
-	
-	public static final String SOFTWARE_NAME = "Ecureuil";
-	public static final String COMPANY_NAME = "SQUIRREL";
-	
-	private JSplitPane horizontalSplitPane;
+	public static final String					SOFTWARE_NAME		= "Ecureuil";
+	public static final String					COMPANY_NAME		= "SQUIRREL";
 
-	/*  LEFT */
-	private JScrollPane filesScrollPane;//vertical scroll
-	private MyListView<MyFile> filesPane;
-	
-	
-	/*  RIGHT */
-	private JScrollPane dbConnectionsScrollPane;
-	private MyListView<MySQLconnection> mySQLconnectionsPane;
-	
-	
+	private JSplitPane							horizontalSplitPane;
 
-	private FileList fileList; //List of the selected files to transform and send. This list must be created by reading the file savingsFile. this list must be modified through add and remove methods.
-	private MySQLconnectionsList mySQLconnectionsList;
+	/* LEFT */
+	private JScrollPane							filesScrollPane;							// vertical
+																							// scroll
+	private MyListView<MyFile>					filesPane;
 
-	
+	/* RIGHT */
+	private JScrollPane							dbConnectionsScrollPane;
+	private MyListView<MySQLconnection>			mySQLconnectionsPane;
+
+	private AbstractDataList<MyFile>			fileList;									// List
+																							// of
+																							// the
+																							// selected
+																							// files
+																							// to
+																							// transform
+																							// and
+																							// send.
+																							// This
+																							// list
+																							// must
+																							// be
+																							// created
+																							// by
+																							// reading
+																							// the
+																							// file
+																							// savingsFile.
+																							// this
+																							// list
+																							// must
+																							// be
+																							// modified
+																							// through
+																							// add
+																							// and
+																							// remove
+																							// methods.
+	private AbstractDataList<MySQLconnection>	mySQLconnectionsList;
+
 	public Home() {
 		super(SOFTWARE_NAME);
 
+		new ApiConnectionPane().display();
+
 		this.fileList = new FileList();
 		this.mySQLconnectionsList = new MySQLconnectionsList();
-		
-		//List of selected directories (left part)
-		/*filesPane = new JPanel(); TODO remove this comment block	
-		filesPane.setLayout(new BoxLayout(filesPane, BoxLayout.Y_AXIS));
-		*/
+
+		// List of selected directories (left part)
+		/*
+		 * filesPane = new JPanel(); TODO remove this comment block
+		 * filesPane.setLayout(new BoxLayout(filesPane, BoxLayout.Y_AXIS));
+		 */
 		filesPane = new MyListView<MyFile>(fileList, new MyFileSupplier());
 		filesScrollPane = new JScrollPane(filesPane);
-		
+
 		mySQLconnectionsPane = new MyListView<MySQLconnection>(mySQLconnectionsList, new MySQLconnectionSupplier());
 		dbConnectionsScrollPane = new JScrollPane(mySQLconnectionsPane);
-		
-		/*  Creation of the horizontal split  */
-		horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-				filesScrollPane, dbConnectionsScrollPane);
+
+		/* Creation of the horizontal split */
+		horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, filesScrollPane, dbConnectionsScrollPane);
 		this.add(horizontalSplitPane);
 
-		
-		
-		//Resizing Listener
+		// Resizing Listener
 		mySQLconnectionsPane.addContainerListener(this);
 		filesPane.addContainerListener(this);
-		
-	
-		//print
-//		this.add(desktop, BorderLayout.CENTER);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// print
+		// this.add(desktop, BorderLayout.CENTER);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(800, 800);
 		this.pack();
 		this.setVisible(true);
 	}
-
 
 	@Override
 	public void componentAdded(ContainerEvent e) {
@@ -89,19 +111,16 @@ public class Home extends JFrame
 		horizontalSplitPane.setDividerLocation(-1);
 	}
 
-
 	@Override
 	public void componentRemoved(ContainerEvent e) {
 		this.revalidate();
 		this.pack();
-		horizontalSplitPane.setDividerLocation(-1);		
+		horizontalSplitPane.setDividerLocation(-1);
 	}
 
-
-	
 	public static void main(String[] args) {
 		Home h = new Home();
-		
+
 	}
 
 }
